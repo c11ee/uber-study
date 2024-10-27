95bbaf01b4a0d0561332cb179703b644260ff62a
cee <1492609616@qq.com>
2024-10-27 21:24:35 +0800

    tailwind，安全区域

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import "@/assets/style/global.css"
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
